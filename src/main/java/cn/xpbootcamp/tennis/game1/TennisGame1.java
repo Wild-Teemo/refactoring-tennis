943e017f1662d3fd8bd3be85f6994153ea4ed9c6
MengyaoLin <mengyao.lin@thoughtworks.com>
2020-03-04 11:45:00 +0800

    simple refactor - remove unused code and rename mysterious name.

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -4,71 +4,64 @@ import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+    private int playScore1 = 0;
+    private int playScore2 = 0;
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals("player1"))
+            playScore1 += 1;
         else
-            m_score2 += 1;
+            playScore2 += 1;
     }
 
     public String getScore() {
-        String score = "";
+        StringBuilder score = new StringBuilder();
         int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
+        if (playScore1 == playScore2) {
+            switch (playScore1) {
                 case 0:
-                    score = "Love-All";
+                    score = new StringBuilder("Love-All");
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score = new StringBuilder("Fifteen-All");
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score = new StringBuilder("Thirty-All");
                     break;
                 default:
-                    score = "Deuce";
+                    score = new StringBuilder("Deuce");
                     break;
 
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+        } else if (playScore1 >= 4 || playScore2 >= 4) {
+            int minusResult = playScore1 - playScore2;
+            if (minusResult == 1) score = new StringBuilder("Advantage player1");
+            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
+            else score = new StringBuilder("Win for player2");
         } else {
             for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
+                if (i == 1) tempScore = playScore1;
                 else {
-                    score += "-";
-                    tempScore = m_score2;
+                    score.append("-");
+                    tempScore = playScore2;
                 }
                 switch (tempScore) {
                     case 0:
-                        score += "Love";
+                        score.append("Love");
                         break;
                     case 1:
-                        score += "Fifteen";
+                        score.append("Fifteen");
                         break;
                     case 2:
-                        score += "Thirty";
+                        score.append("Thirty");
                         break;
                     case 3:
-                        score += "Forty";
+                        score.append("Forty");
                         break;
                 }
             }
         }
-        return score;
+        return score.toString();
     }
 }
